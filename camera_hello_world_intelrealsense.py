9f00d056ec990622b151ec8fa5f69318be7347e7
acharya_praneel <rkacharya258@hotmail.com>
2022-02-15 13:33:09 -0600

    Update camera_hello_world_intelrealsense.py

--- camera_hello_world_intelrealsense.py
+++ camera_hello_world_intelrealsense.py
@@ -1,12 +1,53 @@
 
-import pyrealsense2 as rs
-pipe = rs.pipeline()
-profile = pipe.start()
 
-try:
-  for i in range(0, 100):
-    frames = pipe.wait_for_frames()
-    for f in frames:
-      print(f.profile)
-finally:
-    pipe.stop()
+import pyrealsense2 as rs
+import cv2
+import numpy as np
+import os
+
+
+pipeline = rs.pipeline()
+config = rs.config()
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+pipeline.start(config)
+
+
+
+
+Ncamera_frames = 1000
+
+count = 0
+if __name__ == '__main__':
+
+    # Detect a shape
+    for countframes in range(0, Ncamera_frames):
+
+      # Wait for a coherent pair of frames: depth and color
+      frames = pipeline.wait_for_frames()
+      color_frame = frames.get_color_frame()
+      
+
+      # Convert images to numpy arrays
+      color_image = np.asanyarray(color_frame.get_data())
+
+      cv2.imshow('RealSense', color_image)
+      cv2.waitKey(3)
+
+      filename = os.path.join("Images", "frame_" + str(count) + ".jpg")
+      cv2.imwrite(filename, color_image)
+
+      count = count + 1
